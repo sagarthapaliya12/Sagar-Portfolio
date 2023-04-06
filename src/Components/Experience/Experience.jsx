f5506a835552b2c2d7479b09473d28dabe9c9f08
Sagar Raj Thapaliya <68162024+sagarthapaliya12@users.noreply.github.com>
2023-04-06 19:34:05 +0545

    developed experience section of website

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,10 +1,109 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
-}
+	return (
+		<section id="experience">
+			<h5>Skills That I Have</h5>
+			<h2>My Experience</h2>
 
-export default Experience
+			<div className="container experience_container">
+				<div className="experience_frontend">
+					<h3>Frontend Development</h3>
+					<div className="experience_content">
+						<article className="experience_details">
+							<BsPatchCheckFill className="experience_details-icon"/>
+							<div>
+								<h4>HTML</h4>
+								<small className="text-light">Experienced</small>
+							</div>
+						</article>
+
+						<article className="experience_details">
+							<BsPatchCheckFill className="experience_details-icon"/>
+							<div>
+								<h4>CSS</h4>
+								<small className="text-light">Intermediate</small>
+							</div>
+						</article>
+
+						<article className="experience_details">
+							<BsPatchCheckFill className="experience_details-icon"/>
+							<div>
+								<h4>JavaScript</h4>
+								<small className="text-light">Intermediate</small>
+							</div>
+						</article>
+
+						<article className="experience_details">
+							<BsPatchCheckFill className="experience_details-icon"/>
+							<div>
+								<h4>ReactJs</h4>
+								<small className="text-light">Intermediate</small>
+							</div>
+						</article>
+
+						<article className="experience_details">
+							<BsPatchCheckFill className="experience_details-icon"/>
+							<div>
+								<h4>Redux</h4>
+								<small className="text-light">Beginner</small>
+							</div>
+						</article>
+
+						<article className="experience_details">
+							<BsPatchCheckFill className="experience_details-icon"/>
+							<div>
+								<h4>React-Native</h4>
+								<small className="text-light">Intermediate</small>
+							</div>
+						</article>
+
+						<article className="experience_details">
+							<BsPatchCheckFill className="experience_details-icon"/>
+							<div>
+								<h4>Figma</h4>
+								<small className="text-light">Beginner</small>
+							</div>
+						</article>
+					</div>
+				</div>
+				{/* END OF FRONTEND */}
+				<div className="experience_backend">
+					<div className="experience_frontend">
+						<h3>Backend Development</h3>
+						<div className="experience_content">
+							<article className="experience_details">
+								<BsPatchCheckFill className="experience_details-icon"/>
+								<div>
+									<h4>NodeJS</h4>
+									<small className="text-light">Beginner</small>
+								</div>
+							</article>
+
+							<article className="experience_details">
+								<BsPatchCheckFill className="experience_details-icon"/>
+								<div>
+									<h4>MongoDB</h4>
+									<small className="text-light">Beginner</small>
+								</div>
+							</article>
+
+							<article className="experience_details">
+								<BsPatchCheckFill className="experience_details-icon"/>
+								<div>
+									<h4>PostMan</h4>
+									<small className="text-light">Beginner</small>
+								</div>
+							</article>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Experience;
