9e3b58e77fb527538ec2ec54a8c9ca41fd3616d8
Sagar Raj Thapaliya <68162024+sagarthapaliya12@users.noreply.github.com>
2023-04-06 19:35:40 +0545

    developed portfolio section

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
+import {data} from "./portfolioModel";
+
+import img1 from "../../assets/profilesagar.jpg";
 
 const Portfolio = () => {
-  return (
-    <div>Portfolio</div>
-  )
-}
+	return (
+		<section id="portfolio">
+			<h5>My Recent Work</h5>
+			<h2>Portfolio</h2>
 
-export default Portfolio
+			<div className="container portfolio_container">
+				{data.map(({ id, image, title, github, demo }) => {
+					return (
+						<article key={id} className="portfolio_item">
+							<div className="portfolio_item-image">
+								<img src={image} alt={title} />
+							</div>
+
+							<h3>{title}</h3>
+							<div className="portfolio-cta">
+								<a href={github} className="btn" target="_blank">
+									Github
+								</a>
+
+								<a href={demo} target="_blank" className="btn">
+									Live Demo
+								</a>
+							</div>
+						</article>
+					)
+				})}
+			</div>
+		</section>
+	)
+};
+
+export default Portfolio;
---
+++ src/Components/portfolio/portfolioModel.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Portfolio.css'
+
+import img1 from "../../assets/profilesagar.jpg";
+
+export const data= [
+    {
+        id: 1,
+        image: img1,
+        title: 'BlogNepal',
+        github: 'https://github.com/sagarthapaliya12?tab=repositories',
+        demo: 'https://www.youtube.com/channel/UCLharss4T-377r7GKXVo4Gg',
+    },
+    {
+        id: 2,
+        image: img1,
+        title: 'BlogNepal',
+        github: 'https://github.com/sagarthapaliya12?tab=repositories',
+        demo: 'https://www.youtube.com/channel/UCLharss4T-377r7GKXVo4Gg',
+    },
+    {
+        id: 3,
+        image: img1,
+        title: 'BlogNepal',
+        github: 'https://github.com/sagarthapaliya12?tab=repositories',
+        demo: 'https://www.youtube.com/channel/UCLharss4T-377r7GKXVo4Gg',
+    },
+];
