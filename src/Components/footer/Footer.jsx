5cd8064cd69d0c096b36df5dccac339f835213b7
Sagar Raj Thapaliya <68162024+sagarthapaliya12@users.noreply.github.com>
2023-04-06 19:34:34 +0545

    developed footer section of website

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
+import logo from "../../assets/hd-new-logo.png";
+
+import { BsFacebook } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import { AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+	return (
+		<footer>
+			<a href="#" className="footer_logo">
+				<img src={logo} alt="Logo" className="footer-logo" />
+			</a>
 
-export default Footer
+			<ul className="permalinks">
+				<li>
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#about">About</a>
+				</li>
+				<li>
+					<a href="#experience">Experience</a>
+				</li>
+				<li>
+					<a href="#services">Services</a>
+				</li>
+				<li>
+					<a href="#portfolio">Portfolio</a>
+				</li>
+				<li>
+					<a href="#testimonials">Testimonials</a>
+				</li>
+				<li>
+					<a href="#contact">Contact</a>
+				</li>
+			</ul>
+
+			<div className="footer_socials">
+				<a href="https://www.instagram.com/arko.sagar/" target="_blank">
+					<AiFillInstagram />
+				</a>
+				<a href="https://www.facebook.com/raj.sagar.0511/" target="_blank">
+					<BsFacebook />
+				</a>
+				<a href="https://www.linkedin.com/in/sagar-raj12/" target="_blank">
+					<BsLinkedin />
+				</a>
+			</div>
+			<div className="footer_copyright">
+				<small>&copy; Sagar Raj Thapaliya. All rights reserved.</small>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
