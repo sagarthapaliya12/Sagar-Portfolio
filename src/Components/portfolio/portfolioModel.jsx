46b2d14cfa19849428e59ab28ce7bbbcd6929cfc
Sagar Raj Thapaliya <68162024+sagarthapaliya12@users.noreply.github.com>
2023-04-07 15:12:19 +0545

    edited files in portfolio

--- src/Components/portfolio/portfolioModel.jsx
+++ src/Components/portfolio/portfolioModel.jsx
@@ -1,27 +1,38 @@
 import React from 'react'
 
-import img1 from "../../assets/profilesagar.jpg";
+import img1 from "../../assets/blognepal.jpg";
+import img2 from "../../assets/tradezilla.jpg";
+import img3 from "../../assets/wids.png";
+import img4 from "../../assets/bhojan.jpg";
+
 
 export const data= [
     {
         id: 1,
         image: img1,
         title: 'BlogNepal',
-        github: 'https://github.com/sagarthapaliya12?tab=repositories',
+        github: 'https://github.com/sagarthapaliya12/blogNEPAL',
         demo: 'https://www.youtube.com/channel/UCLharss4T-377r7GKXVo4Gg',
     },
     {
         id: 2,
-        image: img1,
-        title: 'BlogNepal',
-        github: 'https://github.com/sagarthapaliya12?tab=repositories',
+        image: img2,
+        title: 'TradeZilla',
+        github: 'https://github.com/ghimiresaurav/TradeZilla',
         demo: 'https://www.youtube.com/channel/UCLharss4T-377r7GKXVo4Gg',
     },
     {
         id: 3,
-        image: img1,
-        title: 'BlogNepal',
-        github: 'https://github.com/sagarthapaliya12?tab=repositories',
+        image: img3,
+        title: 'WIDS Kathmandu',
+        github: 'https://github.com/sagarthapaliya12/widskathmandu.github.io',
+        demo: 'https://www.youtube.com/channel/UCLharss4T-377r7GKXVo4Gg',
+    },
+    {
+        id: 3,
+        image: img4,
+        title: 'Bhozzan Mobile App',
+        github: 'https://github.com/sagarthapaliya12/bhozzan-client',
         demo: 'https://www.youtube.com/channel/UCLharss4T-377r7GKXVo4Gg',
     },
 ];
